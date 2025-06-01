6c273988796fd88a45771d9ba27a4c76283f5a68
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-02 00:24:37 +0200

    Small name refactor to use hyphen instead of underscore

--- scripts/create-and-run-app.sh
+++ scripts/create-and-run-app.sh
@@ -7,8 +7,8 @@ cd ..
 #docker compose down --rmi all
 
 # Rebuild with no cash to execute all steps fresh
-#docker compose --env-file mariadb_login.env -f compose.yaml build --no-cache
+#docker compose --env-file mariadb-login.env -f compose.yaml build --no-cache
 
 # Rebuild with corrected setup
-docker compose --env-file mariadb_login.env -f compose.yaml build
-docker compose --env-file mariadb_login.env -f compose.yaml up
+docker compose --env-file mariadb-login.env -f compose.yaml build
+docker compose --env-file mariadb-login.env -f compose.yaml up
