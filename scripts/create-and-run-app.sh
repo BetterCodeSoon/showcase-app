e73b102597d7206ecd6ed8f5360dfd79d4cc3196
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-05 15:04:32 +0200

    Improve build and run script

--- scripts/create-and-run-app.sh
+++ scripts/create-and-run-app.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+########################
+# Buildscript Setup
+
+#ENV_FILENAME="" # Empty filename results in docker compose looking for unnamed ".env" file
+ENV_FILENAME="mariadb-login.env"
+ENV_FILE_CMD=""
+
+if [ -n "$ENV_FILENAME" ]; then
+  ENV_FILE_CMD="--env-file $ENV_FILENAME"
+fi
+
+########################
+# Start building..
+
 cd ..
 ./gradlew clean build
 
 # Clean previous builds
 #docker compose down --rmi all
 
-# Rebuild with no cash to execute all steps fresh
-#docker compose --env-file mariadb-login.env -f compose.yaml build --no-cache
+# Build with no cache to execute all steps fresh
+#docker compose ${ENV_FILE_CMD} -f compose.yaml build --no-cache
 
-# Rebuild with corrected setup
-docker compose --env-file mariadb-login.env -f compose.yaml build
-docker compose --env-file mariadb-login.env -f compose.yaml up
+# Build
+docker compose ${ENV_FILE_CMD} -f compose.yaml build
+
+# Start containers
+docker compose ${ENV_FILE_CMD} -f compose.yaml up
