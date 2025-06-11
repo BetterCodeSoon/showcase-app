b51c173a14547a572d1831d9be1953d2c6460c2b
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-11 12:30:05 +0200

    Add sql script that creates nonexisting databases for MariaDB container

---
+++ scripts/initdb/create-databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS showcase_master;
+CREATE DATABASE IF NOT EXISTS showcase_master_integration;
+CREATE DATABASE IF NOT EXISTS showcase_dev;
+CREATE DATABASE IF NOT EXISTS showcase_dev_integration;
+CREATE DATABASE IF NOT EXISTS showcase_debug;
