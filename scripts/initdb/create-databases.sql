4547af5ca83f8293fe9e09c880492790b647dbe2
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-13 14:56:54 +0200

    Refactor script to only include necessary database

--- scripts/initdb/create-databases.sql
+++ scripts/initdb/create-databases.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE IF NOT EXISTS showcase_master;
-CREATE DATABASE IF NOT EXISTS showcase_master_integration;
 CREATE DATABASE IF NOT EXISTS showcase_dev;
-CREATE DATABASE IF NOT EXISTS showcase_dev_integration;
-CREATE DATABASE IF NOT EXISTS showcase_debug;
+-- Add more databases when needed
+-- CREATE DATABASE IF NOT EXISTS showcase_master;
