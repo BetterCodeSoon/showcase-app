7ac1a3bf5de7b47bb210442fc58b91d0b09597d9
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-13 23:45:29 +0200

    Adjust active profile to use global string

--- src/test/java/com/showcase/backend/BackendApplicationTests.java
+++ src/test/java/com/showcase/backend/BackendApplicationTests.java
@@ -1,11 +1,12 @@
 package com.showcase.backend;
 
+import com.showcase.backend.util.SpringProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles(SpringProfiles.TEST)
 class BackendApplicationTests {
 
 	@Test
