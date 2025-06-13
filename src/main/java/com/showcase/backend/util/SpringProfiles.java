ee460c9e14f74d80f95e6323bc78a71991770982
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-13 23:44:47 +0200

    Add helper define spring profile names globally

---
+++ src/main/java/com/showcase/backend/util/SpringProfiles.java
@@ -0,0 +1,8 @@
+package com.showcase.backend.util;
+
+public class SpringProfiles {
+
+  public static final String DEBUG = "debug";
+  public static final String INTEGRATION = "integration";
+  public static final String TEST = "test";
+}
