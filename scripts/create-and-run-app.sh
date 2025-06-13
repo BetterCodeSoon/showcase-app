795c78331a5bc19340840b8e6db3343d1a3f77e0
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-13 12:18:29 +0200

    Improve build script with optional "clean build" parameter and refactor various parts for better readability

--- scripts/create-and-run-app.sh
+++ scripts/create-and-run-app.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+# Parameter "clean" activates full clean gradle build
+
+if [ -z "$1" ]; then
+  echo "Optional usage to activate clean build: $0 clean"
+  echo "Running script with normal build option.."
+fi
+
 ########################
 # Buildscript Setup
 
-# Empty filename results in docker compose looking for unnamed ".env" file
-ENV_FILENAME=""
-#ENV_FILENAME="mariadb-login.env"
-ENV_FILE_CMD=""
+# By default Docker will use the folder name above the compose.yaml as container name.
+# Edit the variable to change to a different name.
+#CONTAINERNAME="showcase-app"
+CONTAINERNAME=""
 
+# By default Docker compose will look for unnamed an ".env" file (same dir as compose.yaml).
+# Edit this variable if you want to switch to a named file.
+# (WARNING: NOT recommended as this can lead to unexpected behaviour!)
+#ENV_FILENAME="mariadb-login.env"
+ENV_FILENAME=""
+
+########################
+# Setting up command line parameters for docker compose
+CONTAINERNAME_PARAM=""
+if [ -n "$CONTAINERNAME" ]; then
+  CONTAINERNAME_PARAM="-p $CONTAINERNAME"
+fi
+
+ENV_FILE_PARAM=""
 if [ -n "$ENV_FILENAME" ]; then
-  ENV_FILE_CMD="--env-file $ENV_FILENAME"
+  ENV_FILE_PARAM="--env-file $ENV_FILENAME"
 fi
 
 ########################
@@ -17,7 +38,13 @@ fi
 
 cd ..
 
-./gradlew clean build
+if [[ "$1" == "clean" ]]; then
+  echo "Running: ./gradlew clean build (full clean build)"
+  ./gradlew clean build
+else
+  echo "Running: ./gradlew build (incremental build)"
+  ./gradlew build
+fi
 
 # Clean previous builds
 #docker compose down --rmi all
@@ -26,10 +53,10 @@ cd ..
 #docker compose down --remove-orphans
 
 # Build with no cache to execute all steps fresh
-#docker compose ${ENV_FILE_CMD} -f compose.yaml build --no-cache
+#docker compose ${ENV_FILE_PARAM} ${CONTAINERNAME_PARAM} -f compose.yaml build --no-cache
 
 # Build
-docker compose ${ENV_FILE_CMD} -f compose.yaml build
+docker compose ${ENV_FILE_PARAM} ${CONTAINERNAME_PARAM} -f compose.yaml build
 
 # Start containers
-docker compose ${ENV_FILE_CMD} -f compose.yaml up
+docker compose ${ENV_FILE_PARAM} ${CONTAINERNAME_PARAM} -f compose.yaml up
