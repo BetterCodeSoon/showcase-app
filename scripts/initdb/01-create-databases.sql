a4e987fb5e6c5e78a758fc4299c7e451769ab9aa
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-15 19:00:33 +0200

    Add option for initdb script to activate auto increment for the AppInfo table

--- scripts/initdb/01-create-databases.sql
+++ scripts/initdb/01-create-databases.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE IF NOT EXISTS showcase_dev;
+-- ensure auto_increment
+--CREATE TABLE app_info (
+--    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--    version VARCHAR(255),
+--    description VARCHAR(255)
+--    -- other fields...
+--);
+
+-- Activate autoincrement after table is generated
+-- ALTER TABLE app_info MODIFY COLUMN id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
 -- Add more databases when needed
 -- CREATE DATABASE IF NOT EXISTS showcase_master;
