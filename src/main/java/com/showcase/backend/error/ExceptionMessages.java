bdb0f1cae5ffab46f52a8e930907e6c5a0a417e3
BetterCodeSoon <bettercodesoon@proton.me>
2025-06-15 19:18:01 +0200

    Add class to define exception messages in one place

---
+++ src/main/java/com/showcase/backend/error/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package com.showcase.backend.error;
+
+public class ExceptionMessages {
+
+  public static final String DELETE_NO_ID = "Delete fails. The id: %s does not exist.";
+  public static final String FIND_NO_ID = "Find fails. The id: %s does not exist.";
+  public static final String UPDATE_NO_ID = "Update fails. The id: %s does not exist.";
+  public static final String ENTITY_NULL = "Entity is null.";
+}
